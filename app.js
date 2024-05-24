23e9ae08b0b4bf683b1a91e1ff0637da7686de05
talyoliv <taly.oliv@gmail.com>
2024-05-24 17:36:48 -0300

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,6 @@ function gerarNumeroAleatorio() {
         return gerarNumeroAleatorio();
     } else {
         listaDeNumerosSorteados.push(numeroEscolhido);
-        console.log(listaDeNumerosSorteados)
         return numeroEscolhido;
     }
 }
